00d988e4f53fcf97f22b737dec26580adf2261aa
minimanimo98 <kgihhh@gmail.com>
2026-03-27 23:14:14 +0900

    Refactor RSS handler to use HTTP client

--- api/rss.js
+++ api/rss.js
@@ -1,16 +1,28 @@
-export default async function handler(req, res) {
+const https = require('https');
+const http = require('http');
+
+module.exports = function handler(req, res) {
   const url = req.query.url;
   if (!url) return res.status(400).send('no url');
-  try {
-    const response = await fetch(url, {
-      headers: { 'User-Agent': 'Mozilla/5.0' }
-    });
-    const text = await response.text();
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Content-Type', 'text/xml; charset=utf-8');
-    res.send(text);
-  } catch(e) {
-    res.status(500).send(e.message);
-  }
-}
 
+  const client = url.startsWith('https') ? https : http;
+
+  const options = {
+    headers: {
+      'User-Agent': 'Mozilla/5.0 (compatible; RSSReader/1.0)',
+      'Accept': 'application/rss+xml, application/xml, text/xml'
+    }
+  };
+
+  client.get(url, options, (r) => {
+    let data = '';
+    r.on('data', chunk => data += chunk);
+    r.on('end', () => {
+      res.setHeader('Access-Control-Allow-Origin', '*');
+      res.setHeader('Content-Type', 'text/xml; charset=utf-8');
+      res.send(data);
+    });
+  }).on('error', (e) => {
+    res.status(500).send(e.message);
+  });
+};
